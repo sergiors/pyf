6afc6a8d394d0f8b608b99bd24049cb296dfa904
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2018-10-05 17:33:50 -0300

    WIP - Reduce

--- README.md
+++ README.md
@@ -4,6 +4,20 @@
     >>> [1, 2, 3] | prepend(1)
     [1, 1, 2, 3]
 
-
     >>> {'foo': 'bar'} | merge({'bar': 'foo'})
     {'foo': 'bar', 'bar': 'foo'}
+
+    >>> {'name': 'Joe'} | prop('name')
+    'Joe'
+
+    >>> {'addr': {'country': 'Brazil'}} | path(['addr', 'country'])
+    'Brazil'
+
+    >>> [1, 2, 3] | map(lambda x: x + 2)
+    [3, 4, 5]
+
+    >>> [-1, 1, 2, -2, 3] | filter(lambda x: x > 0)
+    [1, 2, 3]
+
+    >>> [1, 2, 3, 4, 5] | reduce(lambda x, y: x+y)
+    15
--- pyf.py
+++ pyf.py
@@ -4,12 +4,43 @@ from pipe import Pipe
 
 
 @Pipe
+def prop(dct: dict, k: str, default=None):
+    return dct.get(k, default)
+
+
+@Pipe
 def prepend(lst: list, elm) -> list:
     return [elm] + lst
+
 
 @Pipe
 def merge(l_dct: dict, r_dct: dict) -> dict:
     return {**l_dct, **r_dct}
+
+
+@Pipe
+def path(dct: dict, steps: list):
+    k = steps[0]
+    v = dct.get(k)
+
+    return v if type(v) is not dict else v | path(steps[1:])
+
+
+@Pipe
+def map(xs: list, f) -> list:
+    return [f(x) for x in xs]
+
+
+@Pipe
+def filter(xs: list, f) -> list:
+    return [x for x in xs if f(x)]
+
+
+@Pipe
+def reduce(xs: list, f, init=None):
+    from functools import reduce
+
+    return reduce(f, xs, init)
 
 
 if __name__ == '__main__':
