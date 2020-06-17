25129716a9c39e5f7ec8118b0fa762976079fc6d
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2020-06-17 20:59:52 -0300

    Update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
 
 setup(
     name='pyf',
-    version='0.1',
-    long_description=open('README.md').read(),
-    license='MIT',
     py_modules=['pyf'],
+    version='0.1.0',
+    author='Sérgio Rafael Siqueira',
+    author_email='osergiosiqueira@gmail.com',
+    url='https://github.com/sergiors/pyf',
+    download_url='https://github.com/sergiors/pyf/tarball/master',
+    long_description=readme,
+    long_description_content_type='text/markdown; charset=UTF-8',
+    license='MIT license',
     python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
     install_requires=[
-        "pipe==1.5.0"
+        'pipe==1.5.0'
     ],
 )
